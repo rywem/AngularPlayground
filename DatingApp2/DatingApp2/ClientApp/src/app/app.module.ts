68b93d72ac0e30084a2cba0cf146d1d614417b45
Ryan <cybervoid41@gmail.com>
2020-11-29 11:58:33 -0800

    Messages, Members\MemberList, Members\MemberDetail, Lists added.  Routing added

--- DatingApp2/DatingApp2/ClientApp/src/app/app.module.ts
+++ DatingApp2/DatingApp2/ClientApp/src/app/app.module.ts
@@ -2,35 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+//import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
+//import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component'
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CounterComponent,
+    //CounterComponent,
     FetchDataComponent,
     NavComponent,
-    RegisterComponent
+    RegisterComponent,
+    ListsComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    MessagesComponent
+
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
+    /*RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ]),
-    BrowserAnimationsModule
+    ]),*/
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
