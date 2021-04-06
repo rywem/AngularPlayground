3f61a1c30ca97cfb1726ad0e7b19ec1f06be2627
Ryan <cybervoid41@gmail.com>
2021-04-06 07:17:12 -0700

    Kanban board completed

--- Kanban/src/app/pages/main-view/main-view.component.ts
+++ Kanban/src/app/pages/main-view/main-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -12,21 +14,32 @@ export class MainViewComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  todo = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
-
-  doneList = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
-
+  board: Board = new Board("Test Board", [
+    new Column("Ideas", [
+      "Some random idea",
+      "This another random idea",
+      "Build an application"
+    ]),
+    new Column("Research", [
+      "Lorem Ipsum",
+      "Foo",
+      "This was in the research column"
+    ]),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ])
+  ]);
+  
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
