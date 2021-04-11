f8208f51c85d3852a81e70bbfeff280315fd2ea6
Ryan <cybervoid41@gmail.com>
2021-04-11 10:47:10 -0700

    initial project commt

---
+++ SignalR_Angular/HubConfig/MyHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Angular.HubConfig
+{
+    public class MyHub : Hub
+    {
+
+    }
+}
---
+++ SignalR_Angular/readme.md
@@ -0,0 +1,5 @@
+## Nuget Packages
+- `Microsoft.AspNetCore.SignalR` 
+
+## Youtube
+[https://www.youtube.com/watch?v=QENOe4EMIQ0&list=PLvc5Pp3SAqmoZGMATJWNF3Oyqr0WW_s0i](Youtube SignalR and Angular introduction )
